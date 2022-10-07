7e63a2f15e542f87821a584c5b65226e15d0e7b7
João Ramos <joaopsramos231@gmail.com>
2022-10-07 12:41:43 -0300

    stop using an agent to manage the config

--- lib/snap/config.ex
+++ lib/snap/config.ex
@@ -1,12 +1,11 @@
 defmodule Snap.Config do
   @moduledoc false
-  use Agent
 
-  def start_link({name, config}) do
-    Agent.start_link(fn -> config end, name: name)
+  def get(config_name, otp_app) do
+    Application.get_env(:snap, otp_app)[config_name]
   end
 
-  def get(pid) do
-    Agent.get(pid, & &1)
+  def put_env_config(otp_app, cluster, config) do
+    Application.put_env(:snap, otp_app, [{cluster, config}])
   end
 end
